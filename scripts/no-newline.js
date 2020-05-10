79d3378e95fa29010a368dc8b1d3074de6a662e3
Zee <zeeatgit@gmail.com>
2020-05-09 22:41:24 -0500

    No spaces everywhere

--- scripts/no-newline.js
+++ scripts/no-newline.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 
 let input = fs.readFileSync(__dirname+'/../tmp/index.html', 'utf8')
 if (input) {
-	fs.writeFileSync(__dirname+'/../tmp/stripped.html', input.replace(/\r*\n*/g,' '))
+	fs.writeFileSync(__dirname+'/../tmp/stripped.html', input.replace(/\r*\n+/g,' '))
 } else {
 	throw error('File not found')
 }
